7df185f1bca73f161f06b68df5edde22cc51e74f
bkata64 <64857455+bkata64@users.noreply.github.com>
2023-01-08 21:25:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+app.use(express.static("public"));
+
+const cors = require('cors');
+
+app.use(cors({
+  origin : 'http://localhost:3000'   
+}));
+
+// Read
+app.get('/dogs', (req, res) => {
+    fs.readFile('./data/kutyak.json', (err, file) => {
+        res.send(file);
+    });   
+});
+
+// Read by id
+app.get("/dogs/:egyediAzonosito", (req, res) => {
+    const id = req.params.egyediAzonosito;
+  
+    fs.readFile("./data/kutyak.json", (err, file) => {
+      const dogs = JSON.parse(file);
+      const dogById = dogs.find((dog) => dog.id === id);
+  
+      if (!dogById) {
+        res.status(404);
+        res.send({ error: `id: ${id} not found` });
+        return;
+      }
+  
+      res.send(dogById);
+    });
+  });
+
+ 
+  // Create
+app.post("/dogs", bodyParser.json(), (req, res) => {
+    console.log(req.body);
+    const newDog = {
+      id: uuidv4(),
+      name: sanitizeString(req.body.name),      
+      description: sanitizeString(req.body.description),         
+      kepUrl: req.body.kepUrl,
+    };
+  
+    fs.readFile("data/kutyak.json", (err, file) => {
+      const dogs = JSON.parse(file);
+      dogs.push(newDog);
+      fs.writeFile("data/kutyak.json", JSON.stringify(dogs), (err) => {
+        res.send(newDog);
+      });
+    });
+  });
+
+
+  app.listen(9000);
+
+function sanitizeString(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, "");
+    return str.trim();
+  }
+  
+function uuidv4() {
+return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+    v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+})};
